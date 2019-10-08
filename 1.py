23004e3f26864b48653af42db36d6bc5b12d40ad
MIO1100 <melnichenko739@mail.ru>
2019-10-08 13:14:35 +0700

    наладил визуал и выбор таблицы правил

--- 1.py
+++ 1.py
@@ -9,7 +9,7 @@ import ports_nmap
 import threading
 import subprocess
 import re
-
+# from some_table import One_of_some
 class Main(tk.Frame):
 
     def __init__(self, root):
@@ -19,58 +19,38 @@ class Main(tk.Frame):
     def init_main(self):
         # self.my_threads=[]
 
-        toolbar = tk.Frame(bg='#d7d8e0', bd=2)
-        toolbar.pack(side=tk.TOP, fill=tk.X)
-        btn_open_dialog = tk.Button(toolbar, text='Add', command=self.open_dialog, bg='#d7d8e0', bd=0,
+        self.toolbar = tk.Frame(bg='#d7d8e0', bd=2)
+        self.btn_open_dialog = tk.Button(self.toolbar, text='Add', command=self.open_dialog, bd=0,
                                     compound=tk.TOP)
-        btn_open_dialog.pack(side=tk.LEFT)
         # opened ports
         self.txt_system_param = tk.StringVar()
         self.txt_system_param.set("NON")
         self.ports = tk.Scrollbar(self)
         self.list = tk.Listbox(yscrollcommand=self.ports.set, width=100, height=5)
-        self.list.place(x=30, y=80)
 
         self.label_system_param = tk.Label(self, textvariable=self.txt_system_param)
-        self.label_system_param.grid()
 
         self.iptables_bool = tk.BooleanVar()
         self.check = ttk.Checkbutton(text= "firewall status", variable=self.iptables_bool, command= self.off_ok_iptables)
-        self.check.place(x=10, y=180)
-        self.check_iptables()
-        # self.rules()
-        # x2 = threading.Thread(target=self.check_iptables)
-        # x2.start()
 
+
+        self.str_table=tk.StringVar()
+        self.tables=ttk.Combobox(self, textvariable=self.str_table)
+
+
+        self.toolbar.pack(side=tk.TOP, fill=tk.X)
+        self.btn_open_dialog.pack(side=tk.LEFT)
+        self.list.pack(side=tk.TOP, fill=tk.X)
+        self.label_system_param.grid(row=0, column=0, columnspan=4)
+        self.check.place(x=0, y=170)
+        self.tables.grid(row=1, column=1)
+
+        x2 = threading.Thread(target=self.check_iptables)
+        x2.start()
         x = threading.Thread(target=self.update_ip_ports)
         x.start()
-        #################################
         x1 = threading.Thread(target=self.system_parameters)
         x1.start()
-
-        #rules
-        # self.tables = ttk.Combobox(self, state="readonly", command=self.tables_update())
-        # self.tables.place(x=10,y=200)
-        # self._tables
-
-    # self.cbox['values'] =
-
-    def tables_update(self):
-
-        data = subprocess.Popen("sudo nft list ruleset", shell=True, stdout=subprocess.PIPE).communicate()
-        data=str(data[0],"utf-8")
-        trees=re.findall(r"table+?(.*)\{", data)
-        # self.tables['values'] = trees
-        print(trees)
-    # def trees(self):
-    #     self.parent['pady']=200
-    #     self.parent['padx']=10
-    #     self.note = ttk.Notebook(self, width=1000, height=700)
-
-
-
-
-
 
         # about system
 
@@ -87,12 +67,16 @@ class Main(tk.Frame):
         # print(s)
         self.after(100000, self.system_parameters)
 
-    # def rules(self):
-    #     data = subprocess.Popen("sudo nft list ruleset", shell=True, stdout=subprocess.PIPE).communicate()
-    #     self.str_status.set(str(data[0], 'utf-8'))
+    def table_list(self):
+        data = subprocess.Popen("sudo nft list ruleset", shell=True, stdout=subprocess.PIPE).communicate()
+        data = str(data[0], "utf-8")
+        self.tables['values']=re.findall(r"table+?(.*)\{", data)
+
+
 
     def check_iptables(self):
         data = subprocess.Popen("sudo systemctl status nftables", shell=True, stdout=subprocess.PIPE).communicate()
+        self.table_list()
         if (bytearray(b' active') in data[0]):
             self.iptables_bool.set(True)
         elif (bytearray(b'inactive') in data[0]):
@@ -108,6 +92,8 @@ class Main(tk.Frame):
             self.iptables_bool.set(True)
             print(self.iptables_bool.get())
             subprocess.call("sudo systemctl start nftables", shell=True, stdout=subprocess.PIPE)
+        self.table_list()
+
 
     def update_ip_ports(self):
         ip_ports = ports_nmap.start_all()
